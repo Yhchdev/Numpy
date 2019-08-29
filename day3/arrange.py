6cb75c0d6e7245c0232bd4a20d1a68cc600a111b
Yhchdev <630673943@qq.com>
2019-08-29 23:32:04 +0800

    day3，创建数组(列表，元组转化为数组;arrange,等差linspace,等比logspace)

---
+++ day3/arrange.py
@@ -0,0 +1,21 @@
+#%%
+import numpy as np
+
+
+#%%
+a = np.arange(0,8,2)
+print(a)
+
+#%%
+#等差数列 一维数组
+
+b = np.linspace(0,60,31)
+print(b)
+
+#%%
+#等比数列 一维数组
+
+c = np.logspace(0,9,2)
+print(c)
+
+
---
+++ day3/asarray.py
@@ -0,0 +1,12 @@
+#%%
+import numpy as np
+
+
+#%%
+x = [1,2,3]
+
+x1 = [(1,2,3),(4,5,6),(7,8,9)]
+
+a = np.asarray(x1)
+print(a)
+
